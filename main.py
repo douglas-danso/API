459135d553cb0949a9c86063ab9e061bd2ca7957
douglas-danso <dogsteins1@gmail.com>
2024-10-05 18:39:20 +0000

    feat: added root

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, Query
+from fastapi.responses import FileResponse
 import asyncio
-import api  
+import os
+import api  # Assuming api is the module containing DataFetcher
 
 app = FastAPI()
+app.get("/")
+async def root():
+    return {"message": "quotex api is live"}
 
 @app.get("/create")
 async def create(
@@ -12,6 +17,14 @@ async def create(
     period: int = Query(60, description="Candle period in seconds", ge=1),  # Default 60 seconds
     offset: int = Query(100, description="Number of candles to fetch", ge=1)  # Default 100 candles
 ):
+    file_name = 'otc_data.csv'  
+    
     fetcher = api.DataFetcher(email, password)
-    await fetcher.start_fetching(asset, period, offset)  
-    return {"message": "Quotex scraper started"}
+    await fetcher.fetch_and_save_data(asset, period, offset, file_name)
+
+    # Check if file exists
+    if os.path.exists(file_name):
+        return FileResponse(file_name, media_type='text/csv', filename=file_name)
+    else:
+        return {"message": "File not found or data unavailable"}
+
